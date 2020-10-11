530d2827765b1144c2880e3890d4eb7b63d5671c
ms0697310 <ms0697310@outlook.com>
2020-10-11 17:40:11 +0800

    Feat. TouchDelayAdjustScene alpha ver.

--- Assets/Scripts/AudioEngine.cs
+++ Assets/Scripts/AudioEngine.cs
@@ -2,51 +2,72 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Threading.Tasks;
+using System;
+using System.Linq;
 
 public enum TouchStates
 {
     Touched,
+    TouchFailed,
+    TimeOut,
+    TimeOutFailed,
     Disable,
-    Enable
+    Enable,
+    Reset
 }
 public class AudioEngine: MonoBehaviour
 {
     SpriteRenderer sprite;
-
-    private double BPM;
+    public bool improveDelayMode;
+    public TouchStates touchState;
+    private double bpm;
+    public double BPM
+    {
+        get => bpm;
+        set
+        {
+            bpm = value;
+            MsPB = 1000.0 / (this.bpm / 60.0);
+        }
+    }
     // mini second per beat 
     private double MsPB;
     private double delay;
     private double bufferTime;
+    private double lastTouchDelayTime;
+    private List<double> touchDelayTimes;
+    // 在Beat的多久處Reset
+    private double resetStartTime;
+    // 在Beat的多久處結束Reset
+    private double resetEndTime;
     private double time;
-    public TouchStates touchState;
+    private float timeStep;
     AudioSource BGM;
-    public void setBPM(int newBPM) {
-        BPM = newBPM;
-        MsPB = 1000.0 / (this.BPM / 60.0);
-    }
-    public void setBGM() {
-
-    }
  
     // Start is called before the first frame update
     void Start()
     {
+        improveDelayMode = false;
         sprite = GetComponent<SpriteRenderer>();
-        setBPM(120);
-        delay = -200;
+        BPM = 120;
+        delay = 0;
+        lastTouchDelayTime = 0;
+        touchDelayTimes = new List<double>();
         bufferTime = MsPB / 8;
+        SetResetArgs(0.4, 0.6);
         BGM = this.GetComponentInChildren<AudioSource>();
         BGM.Play();
-        time = 0;
+        time = MsPB;
+        timeStep = 0.01f;
+        InvokeRepeating("timer", timeStep, timeStep);
+        touchState = TouchStates.Disable;
     }
-    // Update is called once per frame
-    void Update()
-    {
-        this.time += Time.deltaTime;
-        //Debug.Log((time*1000).ToString()+"," +getMsPB().ToString());
-        if (isEnableTouchTime())
+    void timer() {
+        this.time += 0.01 * 1000;
+        #region 時間更改State
+        if (IsEnableTouchTime())
         {
+            // 進入可以打擊時段
             if (touchState == TouchStates.Disable)
             {
                 touchState = TouchStates.Enable;
@@ -54,47 +75,123 @@ public class AudioEngine: MonoBehaviour
         }
         else
         {
-            touchState = TouchStates.Disable;
+            // Timeout
+            if (touchState == TouchStates.Enable)
+            {
+                touchState = TouchStates.TimeOut;
+            }
+            // reset結束
+            if (touchState == TouchStates.Reset && !IsResetTime())
+            {
+                touchState = TouchStates.Disable;
+            }
+        }
+        if (IsResetTime())
+        {
+            touchState = TouchStates.Reset;
+        }
+        #endregion
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        #region 打擊更改State
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            switch (touchState)
+            {
+                case TouchStates.Disable:
+                        touchState = TouchStates.TouchFailed;
+                    break;
+                case TouchStates.Enable:
+                        touchState = TouchStates.Touched;
+                    break;
+                case TouchStates.TimeOut:
+                        touchState = TouchStates.TimeOutFailed;
+                    break;
+                case TouchStates.Reset:
+                    break;
+                case TouchStates.Touched:
+                    break;
+                case TouchStates.TouchFailed:
+                    break;
+                case TouchStates.TimeOutFailed:
+                    break;
+            }
+            Touch();
         }
 
 
+        #endregion
+        #region 狀態處理
         switch (touchState)
         {
+            case TouchStates.Reset:
+                break;
             case TouchStates.Disable:
                 sprite.color = new Color(1, 1, 1, 1);
                 break;
             case TouchStates.Enable:
-                if (Input.GetKeyDown(KeyCode.Space))
-                {
-                    //避免反覆檢測
-                    touchState = TouchStates.Touched;
-                    Debug.Log(getThisTouchDelayTime());
-                    /*
-                     * 應該會接在這
-                     */
-                }
                 sprite.color = new Color(1, 0, 0, 1);
                 break;
             case TouchStates.Touched:
                 sprite.color = new Color(0, 1, 0, 1);
                 break;
+            case TouchStates.TouchFailed:
+                sprite.color = new Color(0, 0, 0, 1);
+                break;
+            case TouchStates.TimeOutFailed:
+                sprite.color = new Color(1, 1, 1, 1);
+                break;
         }
 
+        #endregion
+    }
+    private void Touch()
+    {
+        lastTouchDelayTime = GetThisTouchDelayTime();
+        touchDelayTimes.Add(lastTouchDelayTime);
+        if (touchDelayTimes.Count > 10)
+        {
+            touchDelayTimes.RemoveAt(0);
+        }
+        if (improveDelayMode && touchDelayTimes.Count == 10)
+        {
+            double newDelay = GetAvgTouchDelayTime();
+            Debug.Log(newDelay);
+            delay = newDelay;
+        }
     }
 
-    bool isEnableTouchTime() {
-        double touchTime = (time * 1000 % MsPB) - delay;
+    // 設定resetState的起始及結束時間(Beat的0~1)
+    private void SetResetArgs(double resetStartTime, double resetEndTime)
+    {
+        this.resetStartTime = MsPB * resetStartTime;
+        this.resetEndTime = MsPB * resetEndTime;
+    }
+    private bool IsEnableTouchTime() {
+        double touchTime = ((time - delay) % MsPB) ;
         return touchTime <= bufferTime || touchTime >= (MsPB - bufferTime);
     }
-    double getThisTouchDelayTime() {
-        double touchTime = (time * 1000 % MsPB) - delay;
-        if (touchTime <= bufferTime)
-        {
-            return (touchTime);
-        }
-        else
-        {
-            return (touchTime - MsPB);
-        }
+    private bool IsResetTime()
+    {
+        double currentTime = ((time - delay) % MsPB);
+        return currentTime <= resetEndTime && currentTime >= resetStartTime;
+    }
+    private double GetThisTouchDelayTime() {
+        double touchTime = (time  % MsPB) ;
+        return (touchTime);
+    }
+    public double GetLastTouchDelayTime() {
+        return lastTouchDelayTime;
+    }
+    public double GetCurrentDelayTime()
+    {
+        return delay;
+    }
+    public double GetAvgTouchDelayTime()
+    {
+        if (touchDelayTimes.Count == 0) return 0;
+        return touchDelayTimes.Average();
     }
 }
---
+++ Assets/Scripts/TouchDelayAdjustScene.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TouchDelayAdjustScene : MonoBehaviour
+{
+    AudioEngine audioEngine;
+    Text avgDelayText;
+    Text lastTouchDelayText;
+    Text currentDelayText;
+    Text isSuccessText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioEngine = this.GetComponentInChildren<AudioEngine>();
+        avgDelayText = transform.Find("Canvas/AvgDelayText").GetComponent<Text>();
+        lastTouchDelayText = transform.Find("Canvas/LastTouchDelayText").GetComponent<Text>();
+        currentDelayText = transform.Find("Canvas/CurrentDelayText").GetComponent<Text>();
+        isSuccessText = transform.Find("Canvas/IsSuccessText").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        avgDelayText.text = "Avg Touch Delay: " + audioEngine.GetAvgTouchDelayTime().ToString();
+        lastTouchDelayText.text = "Last Touch Delay: " + audioEngine.GetLastTouchDelayTime().ToString();
+        currentDelayText.text = "Current Delay: " + audioEngine.GetCurrentDelayTime().ToString();
+        TouchStates touchState = audioEngine.touchState;
+        audioEngine.improveDelayMode = true;
+        switch (touchState)
+        {
+            case TouchStates.Reset:
+                isSuccessText.text = "";
+                break;
+            case TouchStates.Disable:
+                break;
+            case TouchStates.Enable:
+                break;
+            case TouchStates.TimeOut:
+                isSuccessText.text = "TimeOut";
+                break;
+            case TouchStates.Touched:
+                isSuccessText.text = "Success";
+                break;
+            case TouchStates.TouchFailed:
+                isSuccessText.text = "Failed";
+                break;
+            case TouchStates.TimeOutFailed:
+                isSuccessText.text = "Failed";
+                break;
+        }
+    }
+}
