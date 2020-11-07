31a8c2273f7816524d693ef79f7a1ec2db8bfcc8
Sun <a24230928@gmail.com>
2020-11-08 00:55:53 +0800

    feat: 改為單純動畫表演，操作判定等交由 tempo system 處理

--- Assets/Scripts/HittingUI/NoteController.cs
+++ Assets/Scripts/HittingUI/NoteController.cs
@@ -1,45 +1,37 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class NoteController : MonoBehaviour
 {
-    public float BPM;
+    public TempoManager tempoManager;
     public bool hasStarted;
     public GameObject notePrefab;
     public float noteInitPositionX = 6.0f;
     public float movementSpeed = 1.4f;
+    public float generationSpeed = 1.4f;
+    private float notesGenerationInterval;
+    private bool isRunning;
 
     // Start is called before the first frame update
     void Start()
     {
-        BPM /= 60f;
+        isRunning = false;
+        hasStarted = true;
+        notesGenerationInterval = (float)(60.0d / (tempoManager.BPM * generationSpeed));
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!hasStarted)
+        if (hasStarted && !isRunning)
         {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                hasStarted = true;
-                StartCoroutine(GenerateNotes());
-                Time.timeScale = 1;
-            }
-        } 
-        else
-        {
-            if (Input.GetKeyDown(KeyCode.Escape))
-            {
-                hasStarted = false;
-                Time.timeScale = 0;
-            }
+            StartCoroutine(GenerateNotes());
         }
     }
 
     IEnumerator GenerateNotes()
     {
+        isRunning = true;
         while (hasStarted)
         {
             GameObject leftNote = Instantiate(notePrefab, transform);
@@ -48,7 +40,8 @@ public class NoteController : MonoBehaviour
             rightNote.transform.Translate(noteInitPositionX, 0, 0);
             leftNote.transform.Translate(-noteInitPositionX, 0, 0);
 
-            yield return new WaitForSeconds(1.0f / BPM);
+            yield return new WaitForSeconds(notesGenerationInterval);
         }
+        isRunning = false;
     }
 }
--- Assets/Scripts/HittingUI/NoteObject.cs
+++ Assets/Scripts/HittingUI/NoteObject.cs
@@ -1,67 +1,37 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
 
 public class NoteObject : MonoBehaviour
 {
-    public bool canBePressed;
-
     private float movementSpeed;
     private NoteController noteController;
+    private float BPS;
 
     // Start is called before the first frame update
     void Start()
     {
         noteController = GameObject.Find("NoteHolder").GetComponent<NoteController>();
         movementSpeed = noteController.movementSpeed;
+        BPS = (float)(noteController.tempoManager.BPM / 60d);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (canBePressed)
-        {
-            if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow))
-            {
-                GameObject.Find("msg").GetComponent<Text>().text = "Great";
-                Destroy(gameObject);
-            }
-        }
-
         if (transform.position.x > 0)
         {
-            transform.Translate(-noteController.BPM * Time.deltaTime * movementSpeed, 0, 0);
+            transform.Translate(-BPS * Time.deltaTime * movementSpeed, 0, 0);
             if (transform.position.x <= 0)
             {
-                GameObject.Find("msg").GetComponent<Text>().text = "Miss";
                 Destroy(gameObject);
             }
         }
         else if (transform.position.x < 0)
         {
-            transform.Translate(noteController.BPM * Time.deltaTime * movementSpeed, 0, 0);
+            transform.Translate(BPS * Time.deltaTime * movementSpeed, 0, 0);
             if (transform.position.x >= 0)
             {
-                GameObject.Find("msg").GetComponent<Text>().text = "Miss";
                 Destroy(gameObject);
             }
-        }
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag("HitButton"))
-        {
-            canBePressed = true;
-        }
-    }
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.CompareTag("HitButton"))
-        {
-            canBePressed = false;
         }
     }
 }
