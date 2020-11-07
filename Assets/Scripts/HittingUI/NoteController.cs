4e015c80a896a2e8823d76a6b6d353ea0b4131cd
Sun <a24230928@gmail.com>
2020-11-07 18:55:32 +0800

    feat: 節拍條UI

---
+++ Assets/Scripts/HittingUI/NoteController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteController : MonoBehaviour
+{
+    public float BPM;
+    public bool hasStarted;
+    public GameObject notePrefab;
+    public float noteInitPositionX = 6.0f;
+    public float movementSpeed = 1.4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BPM /= 60f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasStarted)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                hasStarted = true;
+                StartCoroutine(GenerateNotes());
+                Time.timeScale = 1;
+            }
+        } 
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                hasStarted = false;
+                Time.timeScale = 0;
+            }
+        }
+    }
+
+    IEnumerator GenerateNotes()
+    {
+        while (hasStarted)
+        {
+            GameObject leftNote = Instantiate(notePrefab, transform);
+            GameObject rightNote = Instantiate(notePrefab, transform);
+
+            rightNote.transform.Translate(noteInitPositionX, 0, 0);
+            leftNote.transform.Translate(-noteInitPositionX, 0, 0);
+
+            yield return new WaitForSeconds(1.0f / BPM);
+        }
+    }
+}
---
+++ Assets/Scripts/HittingUI/NoteObject.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NoteObject : MonoBehaviour
+{
+    public bool canBePressed;
+
+    private float movementSpeed;
+    private NoteController noteController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        noteController = GameObject.Find("NoteHolder").GetComponent<NoteController>();
+        movementSpeed = noteController.movementSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canBePressed)
+        {
+            if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                GameObject.Find("msg").GetComponent<Text>().text = "Great";
+                Destroy(gameObject);
+            }
+        }
+
+        if (transform.position.x > 0)
+        {
+            transform.Translate(-noteController.BPM * Time.deltaTime * movementSpeed, 0, 0);
+            if (transform.position.x <= 0)
+            {
+                GameObject.Find("msg").GetComponent<Text>().text = "Miss";
+                Destroy(gameObject);
+            }
+        }
+        else if (transform.position.x < 0)
+        {
+            transform.Translate(noteController.BPM * Time.deltaTime * movementSpeed, 0, 0);
+            if (transform.position.x >= 0)
+            {
+                GameObject.Find("msg").GetComponent<Text>().text = "Miss";
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("HitButton"))
+        {
+            canBePressed = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("HitButton"))
+        {
+            canBePressed = false;
+        }
+    }
+}
