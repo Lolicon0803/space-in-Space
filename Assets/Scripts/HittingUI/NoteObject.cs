986dc0d1bf29c2f56ca0c5562b450d5556e7163e
Sun <a24230928@gmail.com>
2020-11-24 23:55:15 +0800

    feat: 暫時調整節奏條表現（等待重構）

--- Assets/Scripts/HittingUI/NoteObject.cs
+++ Assets/Scripts/HittingUI/NoteObject.cs
@@ -1,36 +1,37 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class NoteObject : MonoBehaviour
 {
     private float movementSpeed;
     private HittingController hittingController;
     private float BPS;
+    private float moveTime;
 
     // Start is called before the first frame update
     void Start()
     {
         hittingController = GameObject.Find("HittingUI").GetComponent<HittingController>();
         BPS = (float)(hittingController.audioEngine.BPM / 60d);
+        moveTime = BPS * 2;
+        StartCoroutine(Move());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator Move()
     {
-        if (transform.position.x > 0)
+        float timeElapsed = 0;
+
+        Vector3 startPos = transform.position;
+        Vector3 endPos = new Vector3(0, transform.position.y, transform.position.z);
+
+        while (timeElapsed < moveTime)
         {
-            transform.Translate(-BPS * Time.deltaTime * 1.1f, 0, 0);
-            if (transform.position.x <= 0)
-            {
-                Destroy(gameObject);
-            }
+            transform.position = Vector3.Lerp(startPos, endPos, timeElapsed / moveTime);
+            timeElapsed += Time.deltaTime;
+
+            yield return null;
         }
-        else if (transform.position.x < 0)
-        {
-            transform.Translate(BPS * Time.deltaTime * 1.1f, 0, 0);
-            if (transform.position.x >= 0)
-            {
-                Destroy(gameObject);
-            }
-        }
+
+        Destroy(gameObject);
     }
 }
