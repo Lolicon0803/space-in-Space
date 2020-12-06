bf8154d58ace5c18a82c3912e343e9e2079bdc90
吳俊漳 <a0979790220@gmail.com>
2020-12-06 18:55:16 +0800

    Add logo

---
+++ Assets/BackgroundmoveScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundmoveScript : MonoBehaviour
+{
+    private float speed = 0.001f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float new_x = GetComponent<Renderer>().material.GetTextureOffset("_MainTex").x;
+        GetComponent<Renderer>().material.SetTextureOffset("_MainTex", new Vector2(new_x + speed, 0));
+    }
+}
--- Assets/Scripts/Tempo/TempoManager.cs
+++ Assets/Scripts/Tempo/TempoManager.cs
@@ -55,7 +55,7 @@ public class TempoManager : MonoBehaviour
     {
         improveDelayMode = false;
         BPM = beatPerMinute;
-        delay = -150;
+        delay = -100;
         adjustDelay = 0;
         lastTouchDelayTime = 0;
         touchDelayTimes = new List<double>();
