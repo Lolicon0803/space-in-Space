8ca3c04bd359fe795de6fbd242709824c924bed6
DESKTOP-MSI\Fengling <as61002@gmail.com>
2020-12-12 14:43:34 +0800

    相機根據解析度調整大小 簡單的相機跟隨

---
+++ Assets/Scripts/CameraAspect.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAspect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateAspect();
+    }
+
+    public void UpdateAspect()
+    {
+        // 開發時設定大小 * 期望解析度 / 實際解析度
+        Camera.main.orthographicSize = 5.0f * (16.0f / 9.0f) / ((float)Screen.width / Screen.height);
+    }
+}
