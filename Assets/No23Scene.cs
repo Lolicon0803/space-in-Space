8b82b76a1b3283082396da536712588167ee4a7e
吳俊漳 <a0979790220@gmail.com>
2020-12-30 00:55:13 +0800

    12/27會議內容

---
+++ Assets/No23Scene.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class No23Scene : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject textWriter;
+    void Start()
+    {
+        StartCoroutine("PlayHint");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator PlayHint()
+    {
+        yield return null;
+        textWriter.GetComponent<TextWriter>().Init();
+        textWriter.GetComponent<TextWriter>().LoadStory("23-0.txt");
+        textWriter.GetComponent<TextWriter>().NextStory();
+        Player.Singleton.movement.StopMove(false);
+        Player.Singleton.movement.canInput = false;
+    }
+}
